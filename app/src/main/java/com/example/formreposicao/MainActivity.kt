c5e037dd70bf79216ba263ccaf32b739f7e19a53
JoaopedroSassi <joaopedro10813@gmail.com>
2024-06-12 17:46:40 -0300

    Implementacao form na Main Activity

--- app/src/main/java/com/example/formreposicao/MainActivity.kt
+++ app/src/main/java/com/example/formreposicao/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.example.formreposicao
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etName: EditText
+    private lateinit var etProfession: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        etName = findViewById(R.id.etName)
+        etProfession = findViewById(R.id.etProfession)
+        etEmail = findViewById(R.id.etEmail)
+        btnSend = findViewById(R.id.btnSend)
+
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val profession = etProfession.text.toString()
+            val email = etEmail.text.toString()
+
+            val intent = Intent(this, DisplayActivity::class.java).apply {
+                putExtra("EXTRA_NAME", name)
+                putExtra("EXTRA_PROFESSION", profession)
+                putExtra("EXTRA_EMAIL", email)
+            }
+            startActivity(intent)
+        }
     }
 }
